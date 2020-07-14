e41dce6c813ffe541bae2bff55bf2fc3fe75a88a
Mitusha arya <aryamitusha@gmail.com>
2020-07-14 17:49:10 +0530

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,13 +12,15 @@ class Contact extends Component {
     return(
         <Fade bottom>
             <div id="Contact-section">
-                <h2 className="cent-line"> I am available for any new collabs, opportunities and ideas here : </h2>
+                <h2 className="cent-line"> You can contact me here : </h2>
                 <div className="logos">
                     <a href="https://github.com/mitushaa/" target="_blank" rel="noopener noreferrer"><img className="logoc" src={github} alt="github"></img></a>
                     <a href="mailto:aryamitusha@gmail.com" target="_blank" rel="noopener noreferrer"><img className="logoc" src={gmail} alt="gmail"></img></a>
                     <a href="https://linkedin.com/in/mitushaarya/" target="_blank" rel="noopener noreferrer"><img className="logoc" src={linkedin} alt="linkedin"></img></a>
                     <a href="https://medium.com/@mitushaarya/" target="_blank" rel="noopener noreferrer"><img className="logoc" src={medium} alt="medium"></img></a>
                     <a href="https://twitter.com/mitushaarya" target="_blank" rel="noopener noreferrer"><img className="logoc" src={twitter} alt="twitter"></img></a>
+                    <a href="https://www.quora.com/profile/Mitusha-Arya-1" target="_blank" rel="noopener noreferrer"><img className="logoc" src={quora} alt="quora"></img></a>
+
                </div>
             </div>
         </Fade>
