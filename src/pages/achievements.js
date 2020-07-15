e6946e4fdf666bcb8a29f2a754d5b5fa4526c189
Mitusha arya <aryamitusha@gmail.com>
2020-07-15 12:02:38 +0530

    Update achievements.js

--- src/pages/achievements.js
+++ src/pages/achievements.js
@@ -10,7 +10,7 @@ class Achievements extends Component {
         <div id="achievements-page">
             <div className="contain1">
             <Fade bottom>
-                <h3 className="heading"> <a className="number-about" href=" ">01. </a>About Me </h3>
+                <h3 className="heading"> <a className="number-about" href=" ">02. </a>Achievements </h3>
                 <h4 className="achievements-bold"> Hello! I am Mitusha Arya. </h4>
                 <h4 className="achievements-bold"> I am a developer leaning towards front end web development and design..</h4>
                 <h4 className="achievements"> I am currently a 3rd year Undergraduate (2018-22) at <a className="bold-blue" href=" ">Indian Institute of Information Technology, Vadodara </a>pursuing B. Tech. in Information Technology.
